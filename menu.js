00bc9dfe1da591bd8297f9c734922b82290a1958
joker <61692420+stinky-pig@users.noreply.github.com>
2024-07-26 11:05:36 +0800

    menu

--- menu.js
+++ menu.js
@@ -1,9 +1,10 @@
 Code={
-    MenuData:["农家一碗香","辣椒炒肉","杏鲍菇炒肉","土豆丝","土豆片","西红柿炒蛋"],
+    MenuData:["今晚休息","螺狮粉","农家一碗香","辣椒炒肉","杏鲍菇炒肉","土豆丝","土豆片","西红柿炒蛋","蒜香排骨","小炒牛肉","鸡翅","四季豆炒肉","干锅花菜","空心菜","肉末茄子",
+        "香干炒肉","丝瓜鸡蛋汤","玉米排骨汤","火腿鸡蛋","木耳炒肉","凉拌皮蛋","荷兰豆腊肠","麻婆豆腐","煎蛋饼"],
     PushPlus:{
         Send:function(content,success){
             $.ajax({
-                url: "http://www.pushplus.plus/send",
+                url: "https://www.pushplus.plus/send",
                 data: {token: '506a022d6a5a4fbc84f7284da20b6ae0',content:content},
                 type: "POST",
                 dataType: "json",
